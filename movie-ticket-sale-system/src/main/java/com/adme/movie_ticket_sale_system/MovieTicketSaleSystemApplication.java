8c7b740fa85cfc574641168270d8f134cd67f575
daggimar <auka1511@gmail.com>
2025-09-07 18:20:23 +0000

    initial commit

---
+++ movie-ticket-sale-system/src/main/java/com/adme/movie_ticket_sale_system/MovieTicketSaleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.adme.movie_ticket_sale_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketSaleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketSaleSystemApplication.class, args);
+	}
+
+}
---
+++ movie-ticket-sale-system/src/test/java/com/adme/movie_ticket_sale_system/MovieTicketSaleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adme.movie_ticket_sale_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketSaleSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
